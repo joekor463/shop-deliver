2356be32db564160cc8f087125282e02851c9e44
Eugene Korobenkov <63947827+joekor463@users.noreply.github.com>
2025-11-02 11:55:10 +0300

    delivery-shop step 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import Slider from "@/components/Slider/Slider";
+
 
 export default function Home() {
-  return <h1>Главная</h1>
+  return (
+    <main className="w-full mx-auto mb-20">
+      <Slider/>
+    </main>
+  )
 }
