ea06763b7b1c70078dbbc02d3df175de9bd92332
Евгений Коробенков <e.korobenkov@ACUTA.LOCAL>
2025-11-10 22:00:09 +0300

    step 5 completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Slider from "@/components/Slider/Slider";
+import Actions from '@/components/Actions'
+import Slider from "@/components/Slider/Slider"
 
 
 export default function Home() {
   return (
     <main className="w-full mx-auto mb-20">
       <Slider/>
+      <div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col gap-y-20 md:mb-25 xl:mb-30">
+        <Actions />
+      </div>
     </main>
   )
 }
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { motion } from "framer-motion"
-import Image from "next/image"
 import SlideOne from "./SlideOne"
 import SlideTwo from "./SlideTwo"
 
