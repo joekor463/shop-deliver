fe7b8934ae54695ceaabbe0bdc3496a964330338
Eugene Korobenkov <63947827+joekor463@users.noreply.github.com>
2025-11-11 15:09:38 +0300

    delivery-shop step 7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Actions from '@/components/Actions'
+import NewProducts from '@/components/NewProducts'
+import Purchases from '@/components/Purchases'
 import Slider from "@/components/Slider/Slider"
+import SpecialOffers from '@/components/SpecialOffers'
 
 
 export default function Home() {
@@ -8,6 +11,9 @@ export default function Home() {
       <Slider/>
       <div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col gap-y-20 md:mb-25 xl:mb-30">
         <Actions />
+        <NewProducts />
+        <Purchases />
+        <SpecialOffers />
       </div>
     </main>
   )
--- src/types/product.ts
+++ src/types/product.ts
@@ -4,8 +4,9 @@ export interface ProductCardProps {
 	title: string;
 	description: string;
 	basePrice: number;
-	discountPercent: number;
+	discountPercent?: number;
 	rating: number;
+	categories: string[]
 	weight?: string;
 	volume?: string;
 }
